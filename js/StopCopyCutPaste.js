8f250f5fe60ffaf9166090c8880780515fcf4b5a
Xiaobo <xiaobo.xiao@gmail.com>
2017-05-22 03:05:56 -0700

    added StopCopyCutPaste.js

---
+++ js/StopCopyCutPaste.js
@@ -0,0 +1,14 @@
+<script src="jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+    //Disable cut copy paste
+    $('body').bind('cut copy paste', function (e) {
+        e.preventDefault();
+    });
+   
+    //Disable mouse right click
+    $("body").on("contextmenu",function(e){
+        return false;
+    });
+});
+</script>
