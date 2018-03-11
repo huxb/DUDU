21a782638062156e0cd71fe71d56e8e07b89f07e
XiaoBo <xiaobo.xiao@gmail.com>
2018-03-11 22:11:20 +0800

    Small changes here and there

--- js/main.js
+++
@@ -1,9 +0,0 @@
-//Shows menu tab
-function myFunction() {
-  var x = documnet.getElementById("topNavBar");
-  if (x.className === "topNav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topNav";
-  }
-}
