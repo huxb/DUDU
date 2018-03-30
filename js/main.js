345fd83caa55066032901bee3fc9b8d14baeba8e
XiaoBo <xiaobo.xiao@gmail.com>
2018-03-31 01:11:29 +0800

    made contentSkirt responsive instead of hardcoding height

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-function() {
-  $("#content").onload("home.html");
-  $("#content").load(<p>HI</p>);
+'use strict';
+
+window.onload = function(e) {
+    //Set footer skirt to show footer
+    var footer = document.getElementById("foot");
+    var footOffHeight = footer.offsetHeight;
+    var footHeight = footOffHeight + 5 + "px";
+    var contentSkirt = document.getElementById("contentSkirt");
+    contentSkirt.style.minHeight = footHeight;
 }
+
+
+//calls content
+$(function () {
+    $("#content").load("home.html");
+});
