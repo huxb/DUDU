c6a101d6288249e5828704f3f04c5c343ae4b7ed
XiaoBo <xiaobo.xiao@gmail.com>
2018-03-31 18:50:05 +0800

    set contacts in js to make changing them in the future easier

--- js/main.js
+++ js/main.js
@@ -1,6 +1,54 @@
 'use strict';
 
+//Contact information
+var contactEmail = "zhaoqiian@hotmail.com";
+var contactPhone = "6877 0509";
+var contactLocationStreet = "925 Bukit Timah Road";
+var contactLocationUnit = "#08-13";
+var contactLocationPO = "Singapore 589640";
+
+//Social information
+var facebook = "";
+var twitter = "";
+var google = "";
+var rss = "";
+
 window.onload = function(e) {
+    //Set Links
+        //Set Email
+        $(".emailUs").children("a").attr("href","mailto:"+contactEmail);
+        $(".emailUs").children("a").children("span").text(contactEmail);
+
+        //Set Phone
+        var contactPhoneArray = contactPhone.split(' ');
+        var callPhone = "";
+        while(contactPhoneArray.length) {
+            callPhone = contactPhoneArray.pop() + callPhone;
+        }
+        $(".callUs").children("a").attr("href","tel:+65"+callPhone);
+        $(".callUs").children("a").children("span").text("+(65) "+contactPhone);
+
+        //Set Location
+        var contactLocationStreetArray = contactLocationStreet.split(' ');
+        var contactLocationPOArray = contactLocationPO.split(' ');
+        var googleLocation = "";
+        while(contactLocationPOArray.length) {
+            googleLocation = contactLocationPOArray.pop() + " " + googleLocation;
+        }
+        while(contactLocationStreetArray.length) {
+            googleLocation = contactLocationStreetArray.pop() + " " + googleLocation;
+        }
+        $(".locateUs").children("a").attr("href","http://maps.google.com/?q="+googleLocation);
+        $(".locateUs").children("a").html(contactLocationStreet+'<br>'+contactLocationUnit+'<br>'+contactLocationPO);
+
+        //Set Social
+        $("#facebook").attr("href",facebook);
+        $("#twitter").attr("href",twitter);
+        $("#google").attr("href",google);
+        $("#rss").attr("href",rss);
+
+
+
     //Set footer skirt to show footer
     var footer = document.getElementById("foot");
     var footOffHeight = footer.offsetHeight;
